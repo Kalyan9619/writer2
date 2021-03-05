10687ac8fae22b3ba5bc769b086f5d7a57711abb
Kalyan Somalaraju <kalyansomalaraju@gmail.com>
2021-03-06 05:02:24 +0530

    initial commit

---
+++ constants.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * @author Anass Ferrak aka " ThedevilA " <an.ferrak@gmail.com>
+ * GitHub repo: https://github.com/ThedevilA/Instagram-Web-App-MERN-Stack-Clone
+ *
+ */
+
+// JWT Secret Key , will be used to generate Token (must be a unique key)
+const JWT_SECRET = "jhfbjilsehdg/*d-g*f-dsf*+sdfsd";
+
+module.exports = JWT_SECRET;
